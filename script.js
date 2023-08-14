145c765dcb5b45a06b3382e7ca17a7d9f77d7f5e
navneet-chugh <navneetchugh15@gmail.com>
2023-08-14 13:59:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function threeSum(arr, target) {
-// write your code here
-  
+	//write your code here
+    if (arr.length < 3) {
+        return null; // Not enough elements to form a triplet
+    }
+
+    arr.sort((a, b) => a - b); // Sort the array in ascending order
+    let closestSum = Infinity;
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        let left = i + 1;
+        let right = arr.length - 1;
+
+        while (left < right) {
+            const sum = arr[i] + arr[left] + arr[right];
+            
+            if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                closestSum = sum;
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
 }
 
 module.exports = threeSum;
